deeee2c3f6083e248ed29f797c2e04fc5bd0ea4b
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-02-04 12:38:25 +0300

    Imlpement supporting multiple debian repos for build dependencies

--- build-deb-package.sh
+++ build-deb-package.sh
@@ -4,7 +4,16 @@ CONTAINERNAME=sbuild:latest
 CACHEPATH=/var/cache/docker-builder/sbuild
 [ -z "$DIST" ] && DIST=precise
 
-[ -n "$EXTRAREPO" ] && EXTRACMD="apt-add-repo deb $EXTRAREPO"
+if [ -n "$EXTRAREPO" ] ; then
+   EXTRACMD=""
+   OIFS=$IFS
+   IFS='|'
+   for repo in $EXTRAREPO; do
+      EXTRACMD="${EXTRACMD} --chroot-setup-commands=\"apt-add-repo deb $repo\" "
+   done
+   IFS=$OIFS
+fi
+
 if [ `find . -maxdepth 1 -name \*.dsc | wc -l` == 1 ]; then
     SOURCEFILE=`find . -maxdepth 1 -name \*.dsc`
     SOURCEFILE=`basename $SOURCEFILE`
@@ -17,7 +26,7 @@ SOURCEPATH=`pwd`
 docker run ${DNSPARAM} -i -t --privileged --rm -v ${CACHEPATH}:/srv/images:ro \
     -v ${SOURCEPATH}:/srv/source ${CONTAINERNAME} \
     bash -c "( DEB_BUILD_OPTIONS=nocheck /usr/bin/sbuild -d ${DIST} --nolog \
-             --chroot-setup-commands=\"$EXTRACMD\" \
+             $EXTRACMD \
              --chroot-setup-commands=\"apt-get update\" \
              /srv/source/${SOURCEFILE} 2>&1; \
              echo \$? > /srv/build/exitstatus.sbuild ) \
