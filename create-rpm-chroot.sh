0728ff040ced30397acae9bdaff57336c3fad835
Dmitry Burmistrov <dburmistrov@mirantis.com>
2015-02-05 11:39:03 +0300

    Imlpement supporting multiple rpm repos for build dependencies

--- create-rpm-chroot.sh
+++ create-rpm-chroot.sh
@@ -6,5 +6,4 @@ CACHEPATH=/var/cache/docker-builder/mock
 docker run ${DNSPARAM} -i -t --privileged --rm -v ${CACHEPATH}/cache:/var/cache/mock -v ${CACHEPATH}/lib:/var/lib/mock ${CONTAINERNAME} \
     bash -c "chown -R root:mock /var/cache/mock /var/lib/mock; \
              chmod -R g+s /var/cache/mock /var/lib/mock; \
-             sed -i 's|%EXTRAREPOURL%|http://fakeurl/|g' /etc/mock/centos-${DIST}-x86_64.cfg ;\
              su - abuild -c 'mock -r centos-${DIST}-x86_64 -v --init'"
